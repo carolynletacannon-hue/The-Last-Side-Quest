0b34aa5c0dc8432d193f7d5de6376be1690aab81
carolynletacannon-hue <carolynletacannon@gmail.com>
2026-09-05 12:15:49 -0600

    Add Unreal gameplay module

---
+++ Source/TheLastSideQuest/TheLastSideQuest.Build.cs
@@ -0,0 +1,25 @@
+using UnrealBuildTool;
+
+public class TheLastSideQuest : ModuleRules
+{
+    public TheLastSideQuest(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "UMG"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "Slate",
+            "SlateCore"
+        });
+    }
+}
