fae43304850c78d7f328a10c2cc9f0cb22069628
carolynletacannon-hue <carolynletacannon@gmail.com>
2026-09-05 12:16:05 -0600

    Add Unreal game target

---
+++ Source/TheLastSideQuest.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class TheLastSideQuestTarget : TargetRules
+{
+    public TheLastSideQuestTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.Add("TheLastSideQuest");
+    }
+}
